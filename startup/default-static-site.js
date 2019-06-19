7ceb64277510eb47a4465fe073930052fb3b43e1
blimp <patle@microsoft.com>
2019-06-19 20:40:05 +0000

    [blimp] Add node 9.4

--- startup/default-static-site.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var server = express();
-var options = {
-    index: 'hostingstart.html'
-};
-server.use('/', express.static('/opt/startup', options));
-server.listen(process.env.PORT);
